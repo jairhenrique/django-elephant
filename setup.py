77b820961e8f7b7409f4abcb882982751208e3e5
Jair Henrique <jair.henrique@gmail.com>
2015-11-30 21:39:02 -0200

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 setup(
     name='django-elephant',
     license='MIT',
-    version='0.0.1',
+    version='0.0.2',
     install_requires=['Django>=1.4'],
     url='https://github.com/jairhenrique/django-elephant',
     author='Jair Henrique',
